cfdf9f4404f8523baa5ccf2c623e3a34c54404a9
Waqar Ali <w.ali2020838@gmail.com>
2024-07-20 08:16:44 -0700

    First Commit

---
+++ Password-Generator.py
@@ -0,0 +1,15 @@
+import random
+import string
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+def main():
+    length = int(input("Enter the desired length of the password: "))
+    password = generate_password(length)
+    print(f"Generated password: {password}")
+
+if __name__ == "__main__":
+    main()
